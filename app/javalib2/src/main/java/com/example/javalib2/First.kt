c001905d17c49f5d3ef26b0890386404ebd8f004
Kanikatti REDDY <kanikatti.reddy@randstaddigital.com>
2024-03-18 15:13:36 +0530

    Initial commit

---
+++ app/javalib2/src/main/java/com/example/javalib2/First.kt
@@ -0,0 +1,82 @@
+package com.example.javalib2
+
+
+
+fun main()
+{
+    /* println("hello world")
+     var myName : String = "pavan"
+     var college ="JNTUA"
+     college=" MITS"
+
+
+     val id =100 // val is constant(final)
+    // id = 101
+
+     println(" my name is $myName and length is ${myName.length} and id is $id and my college is $college")
+
+     var age : Int = 26
+     var doubleValue : Double = age.toDouble(); // explicitly need to convert the int to double
+
+     */
+    /* var day = 9
+     when(day)
+     {
+         1-> println(" today is monday")
+         2-> {
+             println(" today is tuesday")
+             println("welcome")
+         }
+         3,4,5-> println(" days of the week")
+         in 8..15 -> println(" not days of the week") // to ckeck within the range of the values(range operator)
+         else -> println(" no days entered")
+     }
+
+
+     for(i in 1..10) {
+         println(i) // it will print 1 to 10
+     }
+
+
+     // for (i in 10 ..1)// it will not print 10 to 1
+     for(i in 10 downTo 1) // for printing 10 to 1
+     {
+         println(i)
+     }
+
+     for(i in 10 downTo 1 step 2) // for printing 10 8 6 4 2
+     {
+         println(i)
+     }
+
+     var myarr = arrayOf(11,34,56,89,102)
+     for(i in myarr)
+     {
+         println(i); // here i is printing value
+     }
+   for(i in 0..myarr.size-1)
+   {
+       println(" index is $i and value is ${myarr[i]}")  // here i is index value of array
+   }
+
+
+     var myarr = arrayOf(11,34,56,89,102)
+     myarr.forEach {
+         println(it) // it is the object of foreach to  printing the stream data of array
+     }
+
+     var mylist = mutableListOf<Int>(55,89,34,97) // here <Int> means we are specifying only integer list
+     mylist.add(12)
+
+     mylist.forEach {
+         println(it)
+     }
+
+     */
+    var i=2;
+    while(i<=100)
+    {
+        println(i)
+        i=i+2
+    }
+}
---
+++ app/javalib2/src/main/java/com/example/javalib2/Second.kt
@@ -0,0 +1,155 @@
+package com.example.javalib2
+
+//fun walking :Unit{} //here unit is like void type
+/*
+fun playing()
+{
+    println("playing")
+}
+fun getsum(a:Int ,b:Int):Int
+{
+    return a + b
+}
+// is  is used to check what is the data type
+fun getTypeFun(myobj : Any) : String{
+    return when(myobj)
+    {
+        is String-> " it is a type of string"
+        is Int-> "it is a type of Int"
+        is Boolean-> "it is type boolean"
+        else->" no data "
+    }
+}
+
+fun main()
+{
+    println(getTypeFun("abc"))
+    println(getsum(5,6))
+    // ? as null safety
+    /*
+    var myName:String? = null
+    var college:String? = "JNTUA"
+    college = null
+    var city : String? = "bangalore"
+    println(" length of the city is ${city?.length}")
+    var name = null
+    // !! non null assertion operator
+    var country:String? = null
+    //println("length of country is ${country?.length}") // application will not crash
+    println("length of country is ${country!!.length}") // application will crash and gives error
+    if(country!=null)
+    {
+        println("${country.length}")
+    }
+    else
+    {
+        println("-1")
+    }
+    // elvis operator // if the value is null left side will execute otherwise -1 will execute
+    var res = country?.length ?: -1
+    println(res)
+     */
+
+  /*  var myName:String? = null
+    myName?.let {  // here let will give access to the function if the value is not null
+        println(myName.length)
+    }
+    println("end of the program")
+
+    // as? safe cast operator
+    // if the variables are same cast type there is no need of as?
+    var college: Any? = "abc" // here Any is super class of all classes
+    //var age: Int? = college as Int // here without as? will gives error
+    var age: Int? = college as? Int // with as? it will give null value
+    print(age)
+   */
+
+
+
+}
+
+ */
+
+/*
+fun getData(name : String = "pavan",age : Int = 26)
+
+{
+    println(" my name is $name and my age is $age")
+}
+fun getData()
+{
+    println(" print the data")
+}
+fun main()
+{
+    getData("naveen",29)
+    getData("ajith")
+    getData(age=44)
+    getData()
+}
+
+fun addNumbers(a:Int , b:Int) : Int
+{
+    return a+b
+}
+var myLambdaAdd: (Int , Int)->Int ={a,b->a+b}
+var myLambdaMul: (Int , Int)->Int ={a,b->a*b}
+fun myHigherOrderFun(x : Int , y : Int , myFun:(Int , Int)->Int):Int{
+    var res = myFun(x,y)
+    return res
+}
+
+
+fun tigerEating(food : String)
+{
+    println("tiger is eating  food $food")
+}
+fun myHigherOrderFun(myFavFood : String ,myFun:(String)->Unit)
+{
+    myFun(myFavFood)
+}
+
+
+var myLambdaMul: (Int , Int)->Int ={a,b->a*b}
+fun tigerEating(food : String)
+{
+    println("tiger is eating $food")
+}
+fun myHigherOrderFun( ):(Int , Int)->Int
+{
+    return  myLambdaMul
+}
+ */
+fun findMax(a : Int, b : Int):Int = if(a>b) a else b
+
+fun showArea(length : Int , breadth : Int) : Int
+{
+    fun calculateArea(): Int = length * breadth
+    var area =calculateArea()
+    return area
+}
+fun main()
+{
+    println(showArea(5,3))
+//    var res = findMax(4,5)
+  //  println(res)
+ /*   var myArr = arrayOf(11,22,33,67,34,68,35,89)
+    var evenNum = myArr.filter { it % 2 == 0 }.map { it * 10 }
+    println(evenNum )
+ //   var mapRes = myArr.map { it * 10 }
+ //   println(mapRes)
+
+    myArr.reduce { res, myCurEle ->
+        println("my current element is $myCurEle")
+        println(" the result is $res")
+        res+myCurEle
+
+    }
+    //var res = myHigherOrderFun()
+    //println(res(5,6))
+    //println(myHigherOrderFun("meat",::tigerEating)) // here :: is reflection
+    // println(myLambda(4,5))
+    //println(myHigherOrderFun(2,3, myLambdaAdd))
+
+  */
+}
